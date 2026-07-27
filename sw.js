20539ed77d4f1e405a29f3d396efd202899a9f1c
arvinmemaphu-beep <arvinmemaphu@gmail.com>
2026-07-28 07:07:42 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,35 @@
+const CACHE_NAME = 'gig-song-hits-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', function(event){
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache){ return cache.addAll(ASSETS); })
+  );
+});
+
+self.addEventListener('activate', function(event){
+  event.waitUntil(
+    caches.keys().then(function(keys){
+      return Promise.all(keys.filter(function(k){ return k !== CACHE_NAME; }).map(function(k){ return caches.delete(k); }));
+    }).then(function(){ return self.clients.claim(); })
+  );
+});
+
+self.addEventListener('fetch', function(event){
+  event.respondWith(
+    caches.match(event.request).then(function(cached){
+      return cached || fetch(event.request).then(function(res){
+        var resClone = res.clone();
+        caches.open(CACHE_NAME).then(function(cache){ cache.put(event.request, resClone); });
+        return res;
+      }).catch(function(){ return cached; });
+    })
+  );
+});
